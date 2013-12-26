227f4e728992a42bada51d15247873664e2779c6
John Adams <johnadams@jqatampa.com>
2013-12-26 11:07:18 -0500

    fitting content

---
+++ scripts/modules/fix-broken-markup.ts
@@ -0,0 +1,22 @@
+
+$("//div[@id='copytxt']") {
+    move_to("//div[@id='header']", position("after"))
+}
+$("//div[@id='footer']") {
+    move_to("//div[@id='header']", position("after"))
+}
+$("//div[@id='cartmsg']") {
+    move_to("//div[@id='header']", position("after"))
+}
+$("//div[@id='main']") {
+    move_to("//div[@id='header']", position("after"))
+}
+$("//div[@id='divFiftyOneWelcomeMatParams']") {
+    move_to("//div[@id='header']", position("after"))
+}
+$("//div[@id='dialog']") {
+    move_to("//div[@id='header']", position("after"))
+}
+$("//div[@class='headerbar2']") {
+    move_to("//div[@id='header']", position("after"))
+}
