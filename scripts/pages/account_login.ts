69849f408e7542d1e67b31f5a8ac1e1dbac52bb6
Damon Burke <dburke@64labs.com>
2014-02-25 13:54:51 -0500

    Preliminary work on customer account area.

--- scripts/pages/account_login.ts
+++ scripts/pages/account_login.ts
@@ -1,4 +1,16 @@
 $('//body') {
     add_class('_account-login')
+    $('div[@id="container"]/div[@id="main"]') {
+        remove('div[@class="breadcrumb"]')
+        remove('div[@id="leftcolumn"]')
 
+        $('div[@id="content"]/div[@class="accountlogin"]') {
+            $('div') {
+                wrap('div') {
+                    attribute('class', 'repad')
+                }
+            }
+        }
+
+    }
 }
