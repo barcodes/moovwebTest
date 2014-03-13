c2b471428a3d1168abfc08e8c90ef401735bfe2a
Matthew McClelland <mmcclelland@64labs.com>
2014-03-13 14:50:39 -0400

    more css stuff; formatted review page as well

--- scripts/pages/checkout_review.ts
+++ scripts/pages/checkout_review.ts
@@ -1,3 +1,46 @@
 $('//body') {
     add_class('_checkout-review')
+
+    $('.//div[@class="checkoutplaceorder"]') {
+        $('./table[@class="placeordertableheader"]') {
+            remove('./thead')
+
+            $('./table') {
+                name('div')
+                remove('./tfoot')
+
+                $('./tr') {
+                    name('div')
+
+                    $('./td') {
+                        name('div')
+                    }
+
+                    $('./div[@class="quantitycolumn"]/div') {
+                        inner() {
+                            prepend('<strong>Quantity:</strong> ')
+                        }
+                    }
+
+                    $('./div[@class="unitpricecolumn"]/div[@class="price"]') {
+                        inner() {
+                            prepend('<strong>Unit Price:</strong> ')
+                        }
+                    }
+
+                    $('./div[contains(@class, "itemtotalcolumn")]') {
+                        inner() {
+                            prepend('<strong>Total Price:</strong> ')
+                        }
+                    }
+
+                    wrap_together('./div[position() > 1]', 'div', class:'right-side')
+                }
+            }
+
+            unwrap()
+        }
+
+        remove('./div[@id="rightcolumn"]/div[@class="checkoutminisummary"]')
+    }
 }
