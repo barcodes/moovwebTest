1db27d3ae3adab62423e78f8bdac1940f8a2bee5
Matthew McClelland <mmcclelland@64labs.com>
2014-03-14 12:05:18 -0400

    dev

--- scripts/request_main.ts
+++ scripts/request_main.ts
@@ -1,5 +1,13 @@
+# log('request_main: ' + $path)
+
+# parse_headers() {  # iterate over all the incoming/outgoing headers
+#     log(name())      # log the name of the current cookie in the iteration
+#     log(value())     # log the value of the current cookie in the iteration
+# }
+
 match($path) {
-    with(/\/home_blocks\/hp-blocks.html/i) {
+    with(/\/home_blocks\/hp-blocks\.html/i) {
         set_upstream_host('www.burkesdesign.com')
     }
 }
+
---
+++ scripts/response_main.ts
@@ -0,0 +1,4 @@
+# parse_headers() {  # iterate over all the incoming/outgoing headers
+#     log(name())      # log the name of the current cookie in the iteration
+#     log(value())     # log the value of the current cookie in the iteration
+# }
