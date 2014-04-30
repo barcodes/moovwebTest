d78a0309e0f9971114f86334b1bb2380742f7cea
Naseem Makiya <naseem.makiya@moovweb.com>
2014-04-30 11:13:42 -0700

    slider code removed

--- scripts/pages/home.ts
+++ scripts/pages/home.ts
@@ -1,4 +1,4 @@
-# Place holder/example file
+# # Place holder/example file
 
 $("body") {
     add_class("_home")
@@ -6,47 +6,8 @@ $("body") {
     insert_top("link", type: "text/css", rel:'stylesheet', href:asset('stylesheets/flexslider.css'))
     insert_top("script", type: "text/javascript", src: asset("javascript/jquery.flexslider-min.js"))
 
-    //when there is a main-banner or large-hp-banner
-    $("//div[@id='main-banner']/.. | //div[@id='large-hp-banner']/..") {
-        %divs = $("div") {
-            remove("@style")
-        }
-        remove("@style")
-        match(%divs) {
-            not("1") {
-                remove("div[@id='main-banner'] | div[@id='large-hp-banner']")
-                attributes(id:"_flexslider", class: "flexslider")
-                insert("ul", class: "slides") {
-                    move_here("../div") {
-                        name("li")
-                        remove("@style")
-                    }
-                }
-                copy_here("//div[@class='categorywrapper']", "after") {
-                    remove("@data-ur-toggler-component")
-                }
-            }
-        }
-    }
-    //when there is a slider
-    $("//a[contains(@href, 'best-sellers')][contains(@style, 'url(')]") {
-        create_image_from_bg()
-        add_class("_best-seller-link")
+    log("Logging upstream: "+ $source_host)
 
-        insert_after("div", id:"_flexslider", class: "flexslider") {
-            %slider = this()
-            $("//ul[@id='slider']") {
-                attributes(id: "_slider", class: "slides")
-                remove("@style")
-                $("li") {
-                    inner("")
-                    create_image_from_bg()
-                }
-                move(this(), %slider, position("bottom"))
-            }
-            remove("following-sibling::div")
-        }
-    }
 
     $('//div[@class="home-slot-top"]') {
         $('//div') {
@@ -55,13 +16,6 @@ $("body") {
     }
 
     $('//div[@id="banners-container"]') {
-        attributes(class: 'flexslider')
-        insert('ul', class: 'slides') {
-            move('../div', '.') 
-            $('./div') {
-                name('li')
-            }
-        }
         insert_after('div', id: '_home-buttons') {
             insert('div', class: 'menu-button', id: '_home-show-categories-btn') {
                 insert('span','All Categories')
